e2b1552220a327834cbb0c2a664dbfcc139df7b6
amit kumar <amitkumar.kit2@gmail.com>
2019-08-16 17:12:51 +0530

    removed dummy.java class

--- git-first/src/Dummy.java
+++
@@ -1,9 +0,0 @@
-
-public class Dummy {
-	public static void main(String[] args) {
-		System.out.println("first statement - updated");
-		System.out.println("new  statement added");
-		System.out.println("3 rd  statement added");
-		System.out.println("4 th  statement added");
-	}
-}
