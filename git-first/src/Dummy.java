66b726becec9d3e9646b31dcb02d761b20272e69
amit kumar <amitkumar.kit2@gmail.com>
2019-08-16 12:12:51 +0530

    updated first syso

--- git-first/src/Dummy.java
+++ git-first/src/Dummy.java
@@ -1,7 +1,7 @@
 
 public class Dummy {
 	public static void main(String[] args) {
-		System.out.println("first statement");
+		System.out.println("first statement - updated");
 		System.out.println("new  statement added");
 	}
 }
