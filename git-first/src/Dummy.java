7fa18a918e4298a15110cd17b49899694b535a0b
amit kumar <amitkumar.kit2@gmail.com>
2019-08-16 12:28:02 +0530

    third stament added

--- git-first/src/Dummy.java
+++ git-first/src/Dummy.java
@@ -3,5 +3,6 @@ public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("first statement - updated");
 		System.out.println("new  statement added");
+		System.out.println("3 rd  statement added");
 	}
 }
