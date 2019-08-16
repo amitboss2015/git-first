d771762a4e2ce60069703bb9bed1dda64e263431
amit kumar <amitkumar.kit2@gmail.com>
2019-08-16 17:12:30 +0530

    adapter design pattern added

---
+++ git-first/src/com/amit/designPatterns/adapter/EmployeeClassAdapter.java
@@ -0,0 +1,8 @@
+package com.amit.designPatterns.adapter;
+
+/**
+ * A class adapter, works as Two-way adapter
+ */
+public class EmployeeClassAdapter {
+	
+}
---
+++ git-first/src/com/amit/designPatterns/adapter/EmployeeObjectAdapter.java
@@ -0,0 +1,8 @@
+package com.amit.designPatterns.adapter;
+
+/**
+ * An object adapter. Using composition to translate interface
+ */
+public class EmployeeObjectAdapter {
+		
+}
