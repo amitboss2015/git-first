b5da0c9dfdfb9157a8a0c15f3107fc6b262ed124
amit kumar <amitkumar.kit2@gmail.com>
2019-08-22 10:29:02 +0530

    added composite design pattern

---
+++ git-first/src/com/designPatterns/composite/begin/BinaryFile.java
@@ -0,0 +1,7 @@
+package com.designPatterns.composite.begin;
+
+//Leaf node in composite pattern
+public class BinaryFile {
+	
+	
+}
---
+++ git-first/src/com/designPatterns/composite/begin/Directory.java
@@ -0,0 +1,11 @@
+package com.designPatterns.composite.begin;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//Composite in the composite pattern
+public class Directory {
+	
+	
+}
